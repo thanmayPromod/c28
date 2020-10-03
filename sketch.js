e1ca308c447640c81a60e0e5c1f1948678e9abf2
padmapravee31 <padmapravee31@gmail.com>
2020-10-03 11:18:41 +0530

    ThanmayC27

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog;
+var rope;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +40,15 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    constrainedLog = new Log(230,180,60,PI);
+    var options = {
+        bodyA: bird.body,
+        bodyB: constrainedLog.body,
+        stiffness: 1,
+        length: 50
+    }
+    rope = Constraint.create(options);
+    World.add(world,rope);
 }
 
 function draw(){
@@ -61,4 +74,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    constrainedLog.display();
+    strokeWeight(4);
+    line(bird.body.position.x,bird.body.position.y,constrainedLog.body.position.x,constrainedLog.body.position.y);
 }
