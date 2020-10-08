5187330ef5bdaab6d94422d5d058998b56f66a5d
thanmayPromod <anupromod2009@gmail.com>
2020-10-08 16:38:36 +0530

    hello c29

--- slingshot.js
+++ slingshot.js
@@ -4,7 +4,7 @@ constructor(bodyA,pointB){
         bodyA: bodyA,
         pointB: pointB,
         stiffness: 1,
-        length: 100
+        length: 30
     }
     this.rope = Constraint.create(options);
     this.sling1=loadImage("sprites/sling1.png")
@@ -17,10 +17,23 @@ display(){
 if(this.rope.bodyA){
 var pointA=this.rope.bodyA.position;
 var pointB=this.rope.pointB;
-line(pointA.x,pointA.y,pointB.x,pointB.y)
-}
 image(this.sling1,200,30)
-image(this.sling2,170,30)
+    image(this.sling2,170,30)
+if(pointA.x<220){
+    stroke ("#301608")
+    strokeWeight(10)
+    line(pointA.x-20,pointA.y,pointB.x-10,pointB.y)
+    line(pointA.x-20,pointA.y,pointB.x+30,pointB.y)
+    image(this.sling3,pointA.x-30,pointA.y-10,15,30)   
+}else{
+    stroke ("#301608")
+    strokeWeight(10)
+    line(pointA.x+20,pointA.y,pointB.x-10,pointB.y)
+    line(pointA.x+20,pointA.y,pointB.x+30,pointB.y)
+    image(this.sling3,pointA.x+25,pointA.y-10,15,30)
+}
+
+}
 
 }
 fly(){
